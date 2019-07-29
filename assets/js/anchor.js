fb7a27b455e637202dfe8fb822368bf69636c7b5
Joseph Garnier <garnjose@gmail.com>
2019-07-29 22:58:44 +0200

    transfer web sources to "sources" branch

--- assets/js/anchor.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  return $('h3').each(function(i, el) {
-    const $el = $(el);
-    const id = $el.attr('id');
-    if (id) {
-      return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html('<i class="fa fa-link"></i>'));
-    }
-  });
-});
